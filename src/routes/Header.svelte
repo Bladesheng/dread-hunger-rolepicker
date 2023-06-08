a7e418a3d54ba9170d7102ddabfbc697367f8cfe
Bladesheng <keadr23@gmail.com>
2023-06-08 18:19:50 +0200

    Style navbar

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,19 +1,49 @@
 <script>
   import { base } from "$app/paths";
+  import { page } from "$app/stores";
 </script>
 
 <header>
   <nav>
-    <ul>
+    <ol>
       <li>
-        <a href="{base}/">Home</a>
+        <a class:selected={$page.route.id === "/"} href="{base}/">Home</a>
       </li>
       <li>
-        <a href="{base}/about">About</a>
+        <a class:selected={$page.route.id === "/about"} href="{base}/about">About</a>
       </li>
-    </ul>
+    </ol>
   </nav>
 </header>
 
 <style>
+  header {
+    padding: 16px;
+  }
+
+  ol {
+    display: flex;
+
+    justify-content: center;
+    gap: 16px;
+  }
+
+  a {
+    font-size: 1.1rem;
+    text-decoration: none;
+
+    color: var(--font-color-primary);
+    border-bottom: 2px solid transparent; /* stops buttons from moving when mousing over them */
+  }
+  a.selected {
+    color: var(--font-color-secondary);
+    border-bottom: 2px solid var(--font-color-secondary) !important;
+  }
+  a:hover {
+    border-bottom: 2px solid var(--font-color-primary);
+  }
+  a:active {
+    color: var(--font-color-blue) !important;
+    border-bottom: 2px solid var(--font-color-blue) !important;
+  }
 </style>
