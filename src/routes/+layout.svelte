7eb1482528314fb5db4537dfe926aa6fc1a402fd
Bladesheng <keadr23@gmail.com>
2023-09-13 19:25:15 +0200

    Self host fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,7 @@
   import Header from "./Header.svelte";
   import Footer from "./Footer.svelte";
 
-  import "../app.css";
-  import "./styles.css";
+  import "../styles/app.css";
 </script>
 
 <div
